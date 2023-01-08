e1bcf205f6f70947d1fe98205f46e14ce3ec6efb
Siyam Ahmmed Nahid <62748235+siyamahmmednahid@users.noreply.github.com>
2023-01-08 22:53:57 +0600

    Chat app update

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ChatList.as_view(), name='chat_list'),
 ]
--- ChatApp/views.py
+++ ChatApp/views.py
@@ -4,23 +4,3 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from .serializers import *
 from .models import *
 from django.contrib.auth.models import User
-
-
-class ChatList(ListCreateAPIView):
-    serializer_class = ChatSerializer
-    permission_classes = (IsAuthenticated,)
-    queryset = Chat.objects.all()
-
-    def list(self, request, *args, **kwargs):
-        queryset = Chat.objects.filter(Sender=request.user) | Chat.objects.filter(Receiver=request.user)
-        serializer = ChatDetailSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def create(self, request, *args, **kwargs):
-        serializer = ChatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(Sender=request.user)
-            serializer = ChatDetailSerializer(serializer.instance)
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
